689cd6ff0e5a6fef1c144ec55e40871bd0225cd7
laa66 <max.sclz94@gmail.com>
2025-06-07 12:13:36 +0200

    schema update

--- internal/core/domain/entity/identity.go
+++ internal/core/domain/entity/identity.go
@@ -8,7 +8,6 @@ import (
 type Identity struct {
 	ID       int
 	Mail     string
-	Login    string
 	Password string
 	Date     time.Time
 	Active   bool
@@ -19,6 +18,6 @@ func (i *Identity) TableName() string {
 }
 
 func (i *Identity) String() string {
-	return fmt.Sprintf("Identity [ID: %d, Mail: %s, Login: %s, Password: %s, Date: %s, Active: %t]", 
-	i.ID, i.Mail, i.Login, i.Password, i.Date, i.Active)
+	return fmt.Sprintf("Identity [ID: %d, Mail: %s, Password: %s, Date: %s, Active: %t]", 
+	i.ID, i.Mail, i.Password, i.Date, i.Active)
 }
